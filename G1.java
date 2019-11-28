f99b72850ab4c6f40f08d87567289c76b272a6c9
Soundarya0 <53481890+Soundarya0@users.noreply.github.com>
2019-11-28 21:49:20 +0530

    Create G1.java

---
+++ G1.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+class Rect extends JFrame
+{
+}
+class G1
+{
+	public static void main(String arg[])
+	{
+		Rect r=new Rect();
+		r.setLocation(500,100);
+		r.setTitle("1st Oct, 2015");
+		r.setSize(500,400);
+		r.setVisible(true);
+	}
+}
